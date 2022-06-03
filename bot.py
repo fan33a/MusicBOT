84c351393a65fa4f3baabe40c3b0ef8c84c3cd63
fan33a <42098159+fan33a@users.noreply.github.com>
2022-06-04 02:09:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,24 +9,9 @@ load_dotenv()
 
 
 
-def get_prefix(bot, msg):
-    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
-
-    prefixes = ['s.'] #Your bot prefix(s)
-
-    return commands.when_mentioned_or(*prefixes)(bot, msg)
-
-bot=commands.Bot(command_prefix=get_prefix,description='Multipurpose Discord Bot')
-
-
-
-
-exts=['music'] #Add your Cog extensions here
-
-
 @bot.event
 async def on_ready():
-    song_name='TWICE - What is love?'  #Status name
+    song_name='شب جديد'  #Status name
     activity_type=discord.ActivityType.listening #Status type
     await bot.change_presence(activity=discord.Activity(type=activity_type,name=song_name))
     print(bot.user.name)
