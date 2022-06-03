1f76a3f7ace5f0d52fb06afc9c4d4a8c655e939c
fan33a <42098159+fan33a@users.noreply.github.com>
2022-06-04 02:15:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,24 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 
+
+
+def get_prefix(bot, msg):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    prefixes = ['s.'] #Your bot prefix(s)
+
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
+
+bot=commands.Bot(command_prefix=get_prefix,description='Multipurpose Discord Bot')
+
+
+
+
+exts=['music'] #Add your Cog extensions here
 
 
 @bot.event
